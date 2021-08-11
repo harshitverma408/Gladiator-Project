b109bb681685f672bf553992df07b2e6c2f93479
HimanP <88125473+HimanP@users.noreply.github.com>
2021-08-11 16:27:20 +0530

    Create ResidentialAddressNotFoundException.java

---
+++ ResidentialAddressNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.demo.layer2;
+
+public class ResidentialAddressNotFoundException extends Exception {
+	public ResidentialAddressNotFoundException(String str) {
+		super(str);
+	}
+
+}
